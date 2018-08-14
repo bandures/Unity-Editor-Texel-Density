3df8efca7e63f4989ef8c07d936e942f992980dd
Anton <antonk@unity3d.com>
2018-08-14 12:40:00 +0100

    Initial commit

---
+++ Editor/TexelDensity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[Serializable]
+public class TexelDensity : EditorWindow
+{
+    public bool initialized = false;
+
+    public bool mode = false;
+    public Shader showTexelsShader;
+
+    [MenuItem("UTools/Texel Density")]
+    public static void ShowWindow()
+    {
+        var window = EditorWindow.GetWindow(typeof(TexelDensity)) as TexelDensity;
+        window.Init();
+        window.Show();
+    }
+
+    public void Init()
+    {
+        if (initialized)
+            return;
+
+        initialized = true;
+
+        minSize = new Vector2(200, 200);
+
+        showTexelsShader = Shader.Find("UTools/TexelDensity");
+    }
+
+    public void OnGUI()
+    {
+        EditorGUILayout.HelpBox("UTools texel density viewer visualize texel density in your scene. Maintain even density to elevating your game visual fidelity and minimizing frame stagger.", MessageType.Info);
+        EditorGUILayout.Space();
+
+        GUILayout.Space(3);
+
+        if (GUILayout.Button(mode ? "Deactivate" : "Activate"))
+        {
+            mode = !mode;
+            SetCameraMode(mode);
+        }
+    }
+
+    public void SetCameraMode(bool showTexel)
+    {
+        if (showTexel && showTexelsShader != null && showTexelsShader.isSupported)
+        {
+            SceneView.lastActiveSceneView.SetSceneViewShaderReplace(showTexelsShader, "");
+            SceneView.lastActiveSceneView.Repaint();
+        }
+        else
+        {
+            SceneView.lastActiveSceneView.SetSceneViewShaderReplace(null, "");
+            SceneView.lastActiveSceneView.Repaint();
+        }
+    }
+}
---
+++ Editor/TexelDensity.shader
@@ -0,0 +1,56 @@
+﻿Shader "UTools/TexelDensity"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float4 _MainTex_TexelSize;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// we assume that maximum texture size is 4096
+				// that gives us not-so-linear gradient, but enough to measure difference in texel denisity
+				// you can make 4096 as setup constant, to modify control range
+
+				fixed4 col = fixed4(max(_MainTex_TexelSize.z, _MainTex_TexelSize.w) / 4096, 0, 0, 0);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
